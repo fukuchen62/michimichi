58628c856fa94b1d257e390d2e33543c58da8868
shisyamoCat <105274240+shisyamoCat@users.noreply.github.com>
2022-06-18 13:50:32 +0900

    h

--- WebContent/js/scroll_top.js
+++ WebContent/js/scroll_top.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
         let wh = $(window).height();
         let scrollPos = window.scrollY + wh;
 
-        if(scrollPos > 1500){
-            $(".back-top").css({"right": "-40px"});
-        }
-        else{
-        	$(".back-top").css({"right": "-120px"});
-        }
+		if(document.width() > 767){
+	        if(scrollPos > 1500){
+	            $(".back-top").css({"right": "-40px"});
+	        }
+	        else{
+	        	$(".back-top").css({"right": "-120px"});
+	        }
+	    }
     });
 
     $(".back-top").click(function () {
